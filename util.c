327dfb9d72145e90fe9c66f4119f5654d44b7298
CS107 tools <cs107@cs.stanford.edu>
2020-05-10 13:21:42 -0700

    This project passes 1 of the 7 default sanity check cases.

--- util.c
+++ util.c
@@ -1,7 +1,7 @@
 #include "samples/prototypes.h"
 #include <stdlib.h>
 #include <string.h>
-
+#include <stdio.h>
 /* initial allocation will be for min size, if not big enough, doubles
  * to 64, then 128, then 256, etc. as needed to accommodate the entire line
  * resize-as-you-go, doubling each time.
@@ -15,7 +15,11 @@ char *read_line(FILE *fp) {
 
     if (ungetc(getc(fp), fp) == EOF) return NULL;
     char *buffer = malloc(sizeof(char) * buflen);
-    fgets(buffer, 4 * buflen, fp);
+    while ( fgets(buffer, buflen, fp) != NULL) {
+      buflen *= 2;
+      buffer = realloc(buffer, sizeof(char) * buflen);
+    }
+    // write assert!!
     buffer[strchr(buffer, '\n') - buffer] = '\0';
     return buffer;
 }
