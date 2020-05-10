427d127e9cb267433334a504474844b75f5b0566
CS107 tools <cs107@cs.stanford.edu>
2020-05-10 12:38:13 -0700

    This project passes 1 of the 7 default sanity check cases.

--- mytail.c
+++ mytail.c
@@ -3,13 +3,70 @@
 #include <error.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <assert.h>
+#include <stdbool.h>
 
 #define MAX_NUM_LINES 100000
 #define DEFAULT_NUM_LINES 10
 
 
+void print_slide(FILE *fp, char **window, int n)
+{
+
+  char *newLine;              /* new line that stores a line read from fp */
+  int count = 0;              /* count of lines read */
+  int start = 0;              /* starting index of N lines) */
+
+
+  /* Read n lines fp file */
+  while ((newLine = read_line(fp))) {
+
+    if (count == n) {
+
+      start %= n;
+      free(window[start]);
+      window[start] = newLine;
+      start++;
+
+    } else {
+
+      window[count] = newLine;
+      count++;
+
+    }
+
+  }
+  start--;
+
+  /* print the last (n-(1+start)) and first (start+1) lines */
+  for (int i = start + 1; i <= n - 1; i++) {
+    printf("%s\n", window[i]);
+  }
+  for (int i = 0; i <= start; i++) {
+    printf("%s\n", window[i]);
+  }
+
+  /* free n_lines */
+  for(int i = 0; i < n; i++){
+    free(window[i]);
+  }
+  if(count > MAX_NUM_LINES) {
+    free(window);
+  }
+}
+
 void print_last_n(FILE *fp, int n) {
     // TODO: your implementation
+  char *lines[MAX_NUM_LINES];
+  char **window;
+
+  if(n <= MAX_NUM_LINES){
+    print_slide(fp, lines, n);
+  }else{
+    window = (char **)malloc((n * sizeof(char *)));
+    assert(window != NULL);
+    print_slide(fp, window, n);
+  }
 }
 
 // ------- DO NOT EDIT ANY CODE BELOW THIS LINE (but do add comments!)  -------
