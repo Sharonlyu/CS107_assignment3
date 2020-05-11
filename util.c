fe7dcecf97e4a1b9f59df76433cdbcfad99dc735
CS107 tools <cs107@cs.stanford.edu>
2020-05-10 18:03:08 -0700

    This project passes 5 of the 7 default sanity check cases.

--- util.c
+++ util.c
@@ -19,28 +19,13 @@ char *read_line(FILE *fp) {
       return NULL;
     }
 
-
+    
     while (strchr(buffer, '\n') == NULL) {
-       char new[buflen];
-
-       fgets(new, buflen, fp);
-       if (new == NULL) {
-	 buffer[buflen - 1] = '\n';
-	 break;
-       }
-       if (strchr(new,  '\n') == NULL) { 
-	 
+       
        buffer = realloc(buffer, sizeof(char)* buflen*2);
+       fgets(buffer + buflen, buflen, fp); 
+       buflen *= 2;
 
-       for (int i = 0; i < buflen; i++) {
-	 buffer[i + buflen] = new[i];
-	 	 if (new[i] == '\n') {
-		   break; //	   return buffer;
-	 }
-       }
-
-       buflen *= 2;}
-       else { break;}
      }
 
         buffer[strchr(buffer, '\n') - buffer] = '\0';
